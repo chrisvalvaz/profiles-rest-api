9f9dcb1a7d04479701d88a751016bc41f99ecde3
Chris <chris.valvaz99@gmail.com>
2025-11-22 10:12:11 -0300

    Added HelloApiView

--- profiles_api/models.py
+++ profiles_api/models.py
@@ -53,4 +53,4 @@ class  UserProfile(AbstractBaseUser, PermissionsMixin):
 
     def __str__(self):
         """Return string representation of our user"""
-        return self.emailS
+        return self.email
